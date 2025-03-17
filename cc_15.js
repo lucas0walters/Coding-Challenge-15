80439207893b28f2c0cba439f2facf36986e771a
lucas0walters <lucas.o.walters@gmail.com>
2025-03-16 21:38:18 -0400

    Task 3 - Removing Risk Items.

--- cc_15.js
+++ cc_15.js
@@ -11,6 +11,16 @@ function addRiskItem(riskName, riskLevel, department) { //Write a function addRi
     let riskCard = document.createElement("div"); //Creates a new risk card (div)
     riskCard.innerHTML = `<h3>${riskName}</h3><p>Risk level: <u>${riskLevel}</u><p>Department: <u>${department}</u></p>`;
     riskCard.setAttribute("class", `riskCard ${riskLevel}`); //Assigns a class "riskCard" to each card.
+
+    //Task 3 - Removing Risk Items.
+    const resolveBtn = document.createElement("button"); //A resolve button
+    resolveBtn.textContent = "Resolve"; //sets the text for the button
+    resolveBtn.addEventListener("click", (event) => { //Adding an event listener to the button
+        riskDashboard.removeChild(riskCard) //removeChild ticket from riskDashboard
+        event.stopPropagation(); //Use stopPropagation() in the "Resolve" button’s event handler to prevent bubbling.
+    });
+
+    riskCard.appendChild(resolveBtn) //adds the resolve button using appendChild
     riskDashboard.appendChild(riskCard); //Appends it to the riskDashboard.
 };
 

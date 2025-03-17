aee6202b6fe02178126deb49f142cf1cffcdfd24
lucas0walters <lucas.o.walters@gmail.com>
2025-03-16 21:29:50 -0400

    Task 2 - Adding Risk Items.

--- cc_15.js
+++ cc_15.js
@@ -1,12 +1,20 @@
 //Task 1 - Base Structure Setup.
 
-riskDashboard = document.getElementById("riskDashboard") //Select the riskDashboard container.
+const riskDashboard = document.getElementById("riskDashboard"); //Select the riskDashboard container.
 if (riskDashboard) {
     console.log("riskDashboard loaded"); //Print "Risk Dashboard Loaded" to the console.
-}
+};
 
 //Task 2 - Adding Risk Items.
 
-function addRiskItem(riskName, riskLevel, department) {
-    console.log(riskName, riskLevel, department)
+function addRiskItem(riskName, riskLevel, department) { //Write a function addRiskItem(riskName, riskLevel, department) that:
+    let riskCard = document.createElement("div"); //Creates a new risk card (div)
+    riskCard.innerHTML = `<h3>${riskName}</h3><p>Risk level: <u>${riskLevel}</u><p>Department: <u>${department}</u></p>`;
+    riskCard.setAttribute("class", `riskCard ${riskLevel}`); //Assigns a class "riskCard" to each card.
+    riskDashboard.appendChild(riskCard); //Appends it to the riskDashboard.
 };
+
+//Test Case: Task 2
+
+addRiskItem("Data Breach", "High", "IT");
+addRiskItem("Supply Chain Disruption", "Medium", "Operations");
